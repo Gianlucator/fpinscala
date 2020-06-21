6648de818deac19afb7fe58184ab88788cbad2f6
Gianlucator <gianluca.principini@gmail.com>
2020-06-22 00:40:16 +0200

    Tree exercises

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,13 +1,46 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(v) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(v) => 1
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  // f è la funzione che trasforma le foglie, g la funzione che manipola i nodi interni
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def map2[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)):Tree[B])((l, r) => Branch(l, r):Tree[B])
+
+  def main(args: Array[String]): Unit = {
+    val tree: Tree[Int] = Branch(Branch(Branch(Leaf(2), Branch(Leaf(4), Leaf(12))), Leaf(3)), Leaf(9))
+    println(depth(tree))
+    println(map(tree)(_ + 1))
+    println(map2(tree)(_ + 1))
+  }
 
 }
